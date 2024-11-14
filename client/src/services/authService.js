6d01fa58eb5c893fcfc949d15274db1d1f0224e7
Md Asif Akhtar <asifakhtar028@gmail.com>
2024-11-14 18:57:20 +0530

    bug fix

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:5000";
-
-
+import { baseUrl } from "../lib/contansts";
 
 export const login = async (email, password) => {
     try {
