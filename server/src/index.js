0c97a46d0c7b60b85c3a750fa7560beeb9ca13c3
Md Asif Akhtar <asifakhtar028@gmail.com>
2024-11-14 19:20:27 +0530

    cors

--- server/src/index.js
+++ server/src/index.js
@@ -8,9 +8,16 @@ const { PORT } = require('./configs/config');
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin: ['https://car-management-beta.vercel.app/', 'http://localhost:5173', 'https://car-management-asifakhtar18s-projects.vercel.app/', 'https://car-management-git-main-asifakhtar18s-projects.vercel.app/']
-}));
+
+
+const corsOptions = {
+    origin: ['https://car-management-beta.vercel.app/', 'http://localhost:5173', 'https://car-management-asifakhtar18s-projects.vercel.app/', 'https://car-management-git-main-asifakhtar18s-projects.vercel.app/'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+};
+
+app.use(cors(corsOptions))
 
 app.use('/users', userRoute);
 app.use('/car', carRoute);
